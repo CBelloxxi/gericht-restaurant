3705f43fa09605ee9adbc37d9af1e08f5cc5cebb
Christopher Bellocci <christopher.bellocci@hotmail.com>
2023-05-11 19:42:44 +0100

    completed laurels section, including beautiful wrapping section of text which looks good on all screens.

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
+import { images, data } from "../../constants";
+import { SubHeading } from "../../components";
+
 import './Laurels.css';
 
+const AwardCard = ({ award: {imgUrl, title, subtitle} }) => (
+  <div className='app__laurels_awards-card'>
+    <img src={imgUrl} alt="award" />
+    <div className="app__laurels_awards-cards_content">
+      <p className="p__cormorant" style={{color: '#DCCA87'}}>{title}</p>
+      <p className="p__cormorant">{subtitle}</p>
+    </div>
+  </div>
+)
+
 const Laurels = () => (
-  <div>
+  <div className='app__bg app__wrapper section__padding' id="awards">
+    <div className='app__wrapper_info'>
+      <SubHeading title="Awards & Recognition"/>
+      <h1 className='headtext__cormorant'>Our Laurels</h1>
+
+      <div className='app__laurels_awards'>
+        {data.awards.map((award) => <AwardCard award={award} key={award.title} />)}
+      </div>
+
+    </div>
+
+    <div className='app__wrapper_img'>
+      <img src={images.laurels} alt="laurels" />
+    </div>
     Laurels
   </div>
 );
